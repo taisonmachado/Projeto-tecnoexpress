78e52bd152ec8401bf89794cddf43291de073270
taisonmachado <taison.machado@hotmail.com>
2020-02-19 10:44:45 -0300

    ProjetoFinal

---
+++ Projeto-Tecnoexpress/Projeto-Tecnoexpress/Controller/Controlador.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Projeto_Tecnoexpress.Model;
+
+namespace Projeto_Tecnoexpress.Controller
+{
+    class Controlador
+    {
+        private List<Modulo> listaModulo; //Guarda a lista de Módulos.
+        private List<Funcionalidade> listaFuncionalidade; //Guarda a lista de Funcionalidades.
+        
+        //Variáveis para leitura/escrita nos arquivos.
+        private StreamWriter escrever;
+        private StreamReader ler;
+
+        private Modulo modulo;
+        private Funcionalidade funcionalidade;
+        
+        private static int id; //Variável para incrementar o id automaticamente.
+
+        public Controlador()
+        {
+            listaModulo = new List<Modulo>();
+            listaFuncionalidade = new List<Funcionalidade>();
+
+            this.LerModulos();
+            this.LerFuncionalidades();
+            //Realiza a leitura dos arquivos assim que um objeto da classe Controlador é instanciado.
+        }
+
+        //Endereços dos arquivos de texto.
+        private string caminhoModulo = Directory.GetCurrentDirectory().Replace("bin\\Debug\\netcoreapp3.1", "Arquivos\\Modulos.txt");
+        private string caminhoFunc = Directory.GetCurrentDirectory().Replace("bin\\Debug\\netcoreapp3.1", "Arquivos\\Funcionalidades.txt");
+
+        
+        public void AddModulo(string descricao, bool mod_bas, float valor)
+        {
+            id++; //incrementando id automaticamente.
+
+            modulo = new Modulo();
+            modulo.Id = id;
+            modulo.Descricao_mod = descricao;
+            modulo.Mod_basico = mod_bas;
+            modulo.Valor = valor;
+
+            listaModulo.Add(modulo); //adicionando na lista.
+
+            //Concatena as informações em uma string para ser adicionada no arquivo.
+            string adicionar = id + "|" + descricao + "|" + mod_bas + "|" + valor;
+            
+            escrever = File.AppendText(caminhoModulo);
+
+            escrever.WriteLine(adicionar); //adicionando no arquivo.
+
+            escrever.Close();
+        }
+
+        public void AddFuncionalidade(int idMod, string descricao)
+        {
+
+            funcionalidade = new Funcionalidade();
+            funcionalidade.Id = idMod;
+            funcionalidade.Descricao_func = descricao;
+
+            listaFuncionalidade.Add(funcionalidade); //adiciona na lista.
+
+            //Concatena as informações em uma string para ser adicionada no arquivo.
+            string adicionar = id + "|" + descricao;
+
+            escrever = File.AppendText(caminhoFunc);
+
+            escrever.WriteLine(adicionar); //adiciona no arquivo.
+
+            escrever.Close();
+        }
+        
+        private void LerModulos()
+        {
+            ler = File.OpenText(caminhoModulo);
+            string conteudo;
+            
+            while (!ler.EndOfStream) // Laço é repetido até que chegue ao final do arquivo.
+            {
+                conteudo = ler.ReadLine(); //Realiza a leitura das linhas do arquivo.
+                string[] vetor = conteudo.Split("|"); //Divide a string retornada do arquivo.
+                
+                modulo = new Modulo();
+                modulo.Id = int.Parse(vetor[0]);
+                modulo.Descricao_mod = vetor[1];
+                modulo.Mod_basico = bool.Parse(vetor[2]);
+                modulo.Valor = float.Parse(vetor[3]);
+                //Passa as informações do arquivo para um objeto da classe Modulo.
+
+                listaModulo.Add(modulo); //Adiciona o objeto na lista.
+            }
+
+            id = modulo.Id; //pega id do ultimo elemento do arquivo.
+            ler.Close();
+            
+        }
+
+        private void LerFuncionalidades()
+        {
+            ler = File.OpenText(caminhoFunc);
+            string conteudo;
+
+            while (!ler.EndOfStream) 
+            {
+                conteudo = ler.ReadLine(); 
+                string[] vetor = conteudo.Split("|"); 
+                
+                funcionalidade = new Funcionalidade();
+
+                funcionalidade.Id = int.Parse(vetor[0]);
+                funcionalidade.Descricao_func = vetor[1];
+
+                listaFuncionalidade.Add(funcionalidade); 
+            }
+
+            ler.Close();
+
+        }
+
+        
+        public void ListarModulosEscolhidos(List<int> idModulo)
+        {
+            foreach (int i in idModulo) //Percorre a lista de IDs
+            {
+                Modulo mod = listaModulo.Find(delegate (Modulo m) //Retorna o módulo corresponde ao ID indicado.
+                {
+                    return m.Id == i;
+                });
+                
+                //Retorna lista de funcionalidades do Módulo.
+                List<Funcionalidade> listaFuncMod = listaFuncionalidade.FindAll(delegate (Funcionalidade f)
+                {
+                    return f.Id == mod.Id;
+                });
+
+                Console.WriteLine(mod.Descricao_mod + " | " + mod.Valor);
+               
+                foreach(Funcionalidade f in listaFuncMod)
+                {
+                    Console.WriteLine("      " + f.Descricao_func);
+                }
+            }
+        }
+        
+
+        public void ListarModulos()
+        {
+            foreach(Modulo m in listaModulo)
+            {
+                Console.WriteLine(m.Id + " | " + m.Descricao_mod);
+            }
+        }
+
+        public float Orcamento(List<int> idModulo)
+        {
+            float valorTotal = 0; //Guarda o valor total do orçamento.
+            bool basico = false; //Verifica se já existe um módulo básico no orçamento.
+            
+            foreach (int i in idModulo)
+            { 
+                Modulo mod = listaModulo.Find(delegate (Modulo m)
+                {
+                    return m.Id == i;
+                });
+
+                if (!mod.Mod_basico)
+                {
+                    valorTotal += mod.Valor;
+                }
+                else if (!basico)
+                {
+                    valorTotal += mod.Valor;
+                    basico = true;
+                }
+            }
+
+            return valorTotal;
+        }
+    }
+}
--- Projeto-Tecnoexpress/Projeto-Tecnoexpress/View/Program.cs
+++ Projeto-Tecnoexpress/Projeto-Tecnoexpress/View/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Projeto_Tecnoexpress.Controller;
 using Projeto_Tecnoexpress.Model;
@@ -9,8 +10,75 @@ namespace Projeto_Tecnoexpress
     {
         static void Main(string[] args)
         {
-            Projeto_Tecnoexpress.Controller.Controller controller = new Controller.Controller();
-            controller.ListarModulos();
+
+            Controlador controlador = new Controlador();
+            String decisao;
+            
+            do
+            {
+                Console.WriteLine("Cadastar Módulo [1] | " + "Cadastrar Funcionalidade [2] | " + "Fazer Orçamento [3] | " + "Sair [4]");
+                decisao = Console.ReadLine();
+                Console.Clear();
+                switch (decisao)
+                {
+                    case "1":
+                        bool basico = false; //Ajuda a determinar se o Módulo é básico.
+
+                        Console.WriteLine("Insira uma descrição para o Módulo: ");
+                        String descrição = Console.ReadLine();
+
+                        Console.WriteLine("É um Módulo Básico? [S/N]");
+                        string mod_Bas = Console.ReadLine();
+
+                        if (mod_Bas == "s" || mod_Bas == "S")
+                            basico = true;
+                        else if (mod_Bas == "n" || mod_Bas == "N")
+                            basico = false;
+
+                        Console.WriteLine("Insira um valor para o Módulo: ");
+                        float valor = float.Parse(Console.ReadLine());
+                        controlador.AddModulo(descrição, basico, valor);
+
+                        Console.WriteLine("Ação Concluída. Pressione ENTER para continuar.");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        break;
+                    case "2":
+                        controlador.ListarModulos();
+                        Console.WriteLine("Digite o número do Módulo ao qual deseja adicionar uma Funcionalidade: ");
+                        int id = int.Parse(Console.ReadLine());
+
+                        Console.WriteLine("Insira a descrição da Funcionalidade: ");
+                        string descricao = Console.ReadLine();
+
+                        controlador.AddFuncionalidade(id, descricao);
+
+                        Console.WriteLine("Ação Concluída. Pressione ENTER para continuar.");
+                        Console.ReadLine();
+                        Console.Clear();
+
+                        break;
+                    case "3":
+                        controlador.ListarModulos();
+                        List<int> listaId = new List<int>();
+                        do
+                        {
+                            Console.WriteLine("Insira o número do Módulo que deseja adicionar ao Orçamento: ");
+                            int idModulo = int.Parse(Console.ReadLine());
+
+                            listaId.Add(idModulo);
+
+                            Console.WriteLine("Deseja adicionar outro Módulo? [S/N]");
+                            decisao = Console.ReadLine();
+                        } while (decisao == "S" || decisao == "s");
+
+                        Console.Clear();
+                        controlador.ListarModulosEscolhidos(listaId);
+                        Console.WriteLine("Total: " + controlador.Orcamento(listaId));
+                        break;
+                }
+            } while (decisao != "4"); //Mantém o console rodando enquanto o usuário não digitar 4.
         }
     }
 }
